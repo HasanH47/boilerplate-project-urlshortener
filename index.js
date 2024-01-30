a795bb5e57753c75d0a7deee7eb5f67acd07a90c
HasanH47 <cruweldevil@gmail.com>
2024-01-30 16:04:37 +0700

    change html and js

--- index.js
+++ index.js
@@ -2,61 +2,113 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const BodyParser = require("body-parser");
+const DNS = require("dns");
+
+// configuring body-parser for POST methods
+app.use(
+  BodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
-
-const urlMappings = {}; // In-memory storage for short and long URL mappings
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/api/shorturl", function (req, res) {
-  const { url } = req.body;
+// array of object to map links with an ID
+const URLs = [];
 
-  console.log("Received URL:", url);
+// ID variable to be mapped with
+let id = 0;
 
-  // Validate the URL format
-  const urlRegex = /^(https?):\/\/(\S+\.)?(\S{2,})\.\S{2,}$/;
-  if (!urlRegex.test(url)) {
-    console.error("Invalid URL:", url);
-    return res.json({ error: "Invalid URL" });
+// post request to create the shortened url
+app.post("/api/shorturl", (req, res) => {
+  const { url: _url } = req.body;
+
+  if (_url === "") {
+    return res.json({
+      error: "invalid url",
+    });
   }
 
-  // Generate a short code (you can use a library for this)
-  const shortCode = generateShortCode();
+  let parsed_url;
+  const modified_url = _url.replace(
+    /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
+    ""
+  );
 
-  // Store the mapping
-  urlMappings[shortCode] = url;
+  try {
+    parsed_url = new URL(_url);
+  } catch (err) {
+    return res.json({
+      error: "invalid url",
+    });
+  }
 
-  console.log("URL Mappings:", urlMappings);
+  DNS.lookup(modified_url, (err) => {
+    if (err) {
+      return res.json({
+        error: "invalid url",
+      });
+    } else {
+      const link_exists = URLs.find((l) => l.original_url === _url);
 
-  // Respond with the short code
-  res.json({ original_url: url, short_url: shortCode });
+      if (link_exists) {
+        return res.json({
+          original_url: _url,
+          short_url: id,
+        });
+      } else {
+        // increment for each new valid url
+        ++id;
+
+        // object creation for entry into url
+        const url_object = {
+          original_url: _url,
+          short_url: `${id}`,
+        };
+
+        // pushing each new entry into the array
+        URLs.push(url_object);
+
+        // return the new entry created
+        return res.json({
+          original_url: _url,
+          short_url: id,
+        });
+      }
+    }
+  });
 });
 
-app.get("/api/shorturl/:shortCode", function (req, res) {
-  const { shortCode } = req.params;
+// get request to navigate to the url
+app.get("/api/shorturl/:id", (req, res) => {
+  const { id: _id } = req.params;
 
-  // Check if the short code exists
-  if (urlMappings.hasOwnProperty(shortCode)) {
-    // Redirect to the original URL
-    res.redirect(urlMappings[shortCode]);
+  // finding if the id already exists
+  const short_link = URLs.find((sl) => sl.short_url === _id);
+
+  if (short_link) {
+    return res.redirect(short_link.original_url);
   } else {
-    res.json({ error: "Short URL not found" });
+    return res.json({
+      error: "invalid URL",
+    });
   }
 });
-
-function generateShortCode() {
-  return Math.random().toString(36).substring(2, 8);
-}
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+
+// app.listen(3000, () => {
+//   console.log("Server Running....");
+// });
